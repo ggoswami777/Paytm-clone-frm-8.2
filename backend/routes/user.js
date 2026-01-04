4ed8cbd409ee636835d91f0e86010bf6916e7b05
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-04 12:16:04 +0530

    added the update bulk post route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -103,4 +103,30 @@ router.put("/",authMiddleware,async (req,res)=>{
   }
 })
 
+// Route to get users from the backend, filterable via firstName/lastName
+
+router.get("/bulk",async (req,res)=>{
+  const filter=req.query.filter || "";
+  const users=await User.find({
+    $or:[{
+      firstName:{
+        "$regex":filter
+      }
+    },{
+      lastName:{
+        "$regex":filter
+      }
+    }
+  ]
+  })
+  res.json({
+    user:users.map(user=>({
+      username:user.username,
+      firstName:user.firstName,
+      lastName:user.lastName,
+      _id:user._id
+    }))
+  })
+})
+
 module.exports = router;
