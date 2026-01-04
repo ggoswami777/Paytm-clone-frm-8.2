7ccbd1be4e1c2c7c33da4fee42c240e1b215944a
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-04 13:05:53 +0530

    added account route for transactions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const userRouter=require("./user")
 const router=express.Router();
-
+const accountRouter=require("./account")
 router.use("/user",userRouter);
 module.exports=router;
 // /api/v1/user
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -55,6 +55,12 @@ router.post("/signup", async (req, res) => {
     firstName: req.body.firstName,
     lastName: req.body.lastName
   });
+  const userId=user._id;
+  
+  await Account.create({
+    userId,
+    balance:1+Math.random()*10000
+  })
 
   const token = jwt.sign(
     { userId: user._id },
