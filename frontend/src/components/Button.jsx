7e56de7af840f76c1854b54da332cbf1e2907fad
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-08 19:45:26 +0530

    added signup component

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,3 +1,3 @@
 export function Button({label,onClick}){
-    return <button onClick={onClick} type="button" className="w-full text-white bg-gray-800 hover:bg-gray-600">{label}</button>
+    return <button onClick={onClick} type="button" className="w-full text-white bg-gray-800 py-2 hover:bg-gray-600">{label}</button>
 }
