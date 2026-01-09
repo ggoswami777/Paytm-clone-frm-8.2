98750c2f41a51a615497d5e72ab3bad62987c3ec
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-01-09 22:00:24 +0530

    done with the basic app for sending money

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,10 @@
+import { useNavigate } from "react-router-dom"
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 import axios from "axios"
-
 import React, { useState } from 'react'
 
 const Signup = () => {
@@ -12,6 +12,7 @@ const Signup = () => {
   const [lastName,setLastName]=useState("");
   const [username,setUsername]=useState("");
   const [password,setPassword]=useState("");
+  const navigate=useNavigate();
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
@@ -39,6 +40,7 @@ const Signup = () => {
               password
             })
             localStorage.setItem("token",response.data.token);
+            navigate("/dashboard")
           }} label={"Sign up"} />
         </div>
         <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
